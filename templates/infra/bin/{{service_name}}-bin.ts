550d8f46d72ce6abab15f03baef24904fe29316f
StackSpot Admin <admin@stackspot.com>
2022-06-13 17:38:40 +0000

    Update public repository

--- templates/infra/bin/{{service_name}}-bin.ts
+++ templates/infra/bin/{{service_name}}-bin.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { {{service_name|title|replace("-", "")}}ServiceStack } from '../lib/{{service_name}}-service-stack';
 
 const app = new cdk.App();
--- templates/infra/lib/{{service_name}}-service-stack.ts
+++ templates/infra/lib/{{service_name}}-service-stack.ts
@@ -1,7 +1,8 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class {{service_name|title|replace("-", "")}}ServiceStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
 
